9c8ecf11bbc5af38698c13dadf2d9fbe39f3c0f3
HarryB Dj <bougardharry@gmail.com>
2024-02-20 19:45:03 -0500

    up

---
+++ main.js
@@ -0,0 +1,14 @@
+const images = ['img/1.jpg', 'img/2.jpg']; // Array of image paths
+  let currentIndex = 1; // Index of the current image
+  
+  const backgroundElement = document.querySelector('.morphing-bg');
+
+  function morphBackground() {
+    // Change the background image style directly
+    backgroundElement.style.backgroundImage = `url('${images[currentIndex]}')`;
+    
+    // Increment the index (looping back to the start if necessary)
+    currentIndex = (currentIndex + 1) % images.length;
+  }
+
+ 
