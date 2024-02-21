98d208d0b22cea6952d57e2e5120b75e12a7f8fe
HarryB Dj <bougardharry@gmail.com>
2024-02-20 19:47:54 -0500

    y

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const images = ['img/1.jpg', 'img/2.jpg']; // Array of image paths
+const images = ['/img/1.jpg', '/img/2.jpg']; // Array of image paths
   let currentIndex = 1; // Index of the current image
   
   const backgroundElement = document.querySelector('.morphing-bg');
