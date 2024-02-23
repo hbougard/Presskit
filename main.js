fc5d40f063e4491e16f355aeaef504475138ba2f
HarryB Dj <bougardharry@gmail.com>
2024-02-22 20:20:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
+// Check if device orientation event is supported
+if ('DeviceOrientationEvent' in window) {
+  // Add event listener for device orientation
+  window.addEventListener('deviceorientation', handleOrientation);
+} else {
+  // Fallback mechanism if device orientation is not supported
+  console.log('Device orientation not supported.');
+  // Remove mouse movement event listener
+  document.removeEventListener('mousemove', handleMouseMove);
+}
+
 // Function to handle mouse movement event
 function handleMouseMove(e) {
   const background = document.getElementById('background');
   const rect = background.getBoundingClientRect(); // Get the position of the background element
   const xPos = (e.clientX - rect.left) / rect.width * 100; // Calculate xPos relative to the background element
   const yPos = (e.clientY - rect.top) / rect.height * 100; // Calculate yPos relative to the background element
-  background.style.background = `radial-gradient(circle at ${xPos}% ${yPos}%,  rgba(63,94,251,1) 0%, rgba(70,204,252,1) 18%, rgba(0,0,0,1) 35%)`;
+  background.style.background = `radial-gradient(circle at ${xPos}% ${yPos}%, rgba(63,94,251,1) 0%, rgba(70,204,252,1) 18%, rgba(0,0,0,1) 35%)`;
 }
 
 // Add event listener for mouse movement
@@ -22,15 +33,5 @@ function handleOrientation(event) {
   const yPos = (beta + 50) / 100 * window.innerHeight;
 
   // Apply the gradient with the adjusted position
-  background.style.background = `radial-gradient(circle at ${xPos}px ${yPos}px,  rgba(63,94,251,1) 0%, rgba(70,204,252,1) 18%, rgba(0,0,0,1) 35%)`;
+  background.style.background = `radial-gradient(circle at ${xPos}px ${yPos}px, rgba(63,94,251,1) 0%, rgba(70,204,252,1) 18%, rgba(0,0,0,1) 35%)`;
 }
-
-// Add event listener for device orientation if supported
-if ('DeviceOrientationEvent' in window) {
-  window.addEventListener('deviceorientation', handleOrientation);
-} else {
-  // Fallback mechanism if device orientation is not supported
-  console.log('Device orientation not supported.');
-}
-
-
