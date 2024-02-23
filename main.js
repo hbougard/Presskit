e6aa72e4579ff39adb93f0311f8b6521ae441330
HarryB Dj <bougardharry@gmail.com>
2024-02-22 20:34:58 -0500

    ok

--- main.js
+++ main.js
@@ -24,14 +24,12 @@ document.addEventListener('mousemove', handleMouseMove);
 // Function to handle device orientation event
 function handleOrientation(event) {
   const background = document.getElementById('background');
-  if (!background) return; // Check if background element exists
-  const gamma = event.gamma; // Get the gamma value (tilt from left to right)
-  const beta = event.beta;   // Get the beta value (tilt from front to back)
+  if (!background) return;
+  const gamma = event.gamma;
+  const beta = event.beta;
 
-  // Normalize gamma and beta values to be in the range [0, 100] based on screen dimensions
-  const xPos = (gamma + 90) / 100 * window.innerWidth;
-  const yPos = (beta + 90) / 100 * window.innerHeight;
+  const xPos = (gamma + 50) / 100 * window.innerWidth;
+  const yPos = (beta + 50) / 100 * window.innerHeight;
 
-  // Apply the gradient with the adjusted position
   background.style.background = `radial-gradient(circle at ${xPos}px ${yPos}px, rgba(63,94,251,1) 0%, rgba(70,204,252,1) 18%, rgba(0,0,0,1) 35%)`;
 }
